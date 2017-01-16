36686fc6fcc0bbdf87937a8fab54402e6077fe68
Jesse Sanders <jesse.sanders@briebug.com>
2017-01-16 06:19:06 -0700

    code cleanup:

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,9 +1,5 @@
 import {Component} from '@angular/core';
-
-import {NavController} from 'ionic-angular';
 import {BarcodeScanner} from 'ionic-native';
-
-declare var scanner: any;
 
 @Component({
     selector: 'page-about',
@@ -11,42 +7,22 @@ declare var scanner: any;
 })
 export class AboutPage {
 
-    formats = [
-        'AZTEC',
-        'CODABAR',
-        'CODE_39',
-        'CODE_93',
-        'CODE_128',
-        'DATA_MATRIX',
-        'EAN_8',
-        'EAN_13',
-        'ITF',
-        'MAXICODE',
-        'PDF_417',
-        'QR_CODE',
-        'RSS_14',
-        'RSS_EXPANDED',
-        'UPC_A',
-        'UPC_E',
-        'UPC_EAN_EXTENSION'
-    ];
-
-    constructor(public navCtrl: NavController) {
+    constructor() {
 
     }
 
     scan() {
         BarcodeScanner.scan(
             {
-                preferFrontCamera : false, // iOS and Android
-                showFlipCameraButton : true, // iOS and Android
-                showTorchButton : true, // iOS and Android
+                preferFrontCamera: false, // iOS and Android
+                showFlipCameraButton: true, // iOS and Android
+                showTorchButton: true, // iOS and Android
                 torchOn: true, // Android, launch with the torch switched on (if available)
-                prompt : "Place a barcode inside the scan area", // Android
+                prompt: "Place a barcode inside the scan area", // Android
                 resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                formats : "UPC_E,UPC_A", // default: all but PDF_417 and RSS_EXPANDED
-                orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-                disableAnimations : true // iOS
+                formats: "UPC_E,UPC_A", // default: all but PDF_417 and RSS_EXPANDED
+                orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+                disableAnimations: true // iOS
             }
         ).then(
             function (result) {
@@ -58,13 +34,5 @@ export class AboutPage {
             function (error) {
                 alert("Scanning failed: " + error);
             });
-    }
-
-    close() {
-        // scanner.close();
-    }
-
-    flash() {
-        // scanner.toggleFlash();
     }
 }
