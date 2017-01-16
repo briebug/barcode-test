31cac19d83916a64a724ecff49fce9028efcff97
Jesse Sanders <jesse.sanders@briebug.com>
2017-01-16 13:59:55 -0700

    build(cirleci) - fastlane build final

---
+++ fastlane/select_xcode_signing_method.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/ruby
+
+require 'xcodeproj'
+
+# Usage:
+# ruby select_xcode_signing_method.rb -p <path/to/xcode_project> -t <Target> -m ['Automatic' | 'Manual']
+
+class Options
+  attr_accessor :path, :target, :method
+
+  def initialize(args)
+    @path = args[:path] || Dir.glob("*.xcodeproj").first
+    @target = args[:target]
+    @method = args[:method]
+  end
+end
+
+def parse_args
+  options_hash = {}
+  args = ARGV
+  args.each_with_index do |arg, index|
+    case arg
+    when '--project_path', '-p'
+      path = args[index + 1]
+      unless File.exist?(path)
+        abort('There is no file at specified path.')
+      end
+      options_hash[:path] = path
+    when '--target', '-t'
+      options_hash[:target] = args[index + 1]
+    when '--signing_method', '-m'
+      method = args[index + 1]
+      unless ['Automatic', 'Manual'].include?(method)
+        abort("'Invalid signing method specified. Please use either 'Automatic' or 'Manual'")
+      end
+      options_hash[:method] = method
+    end
+  end
+
+  options_hash
+end
+
+options = Options.new parse_args
+
+project = Xcodeproj::Project.open(options.path)
+
+target_id = project.targets.select {|target| target.name == options.target }.first.uuid
+project.root_object.attributes['TargetAttributes'] = { target_id => { "ProvisioningStyle" => "Automatic" }} unless project.root_object.attributes.nil?
+attributes = project.root_object.attributes['TargetAttributes']
+target_attributes = attributes[target_id]
+target_attributes['ProvisioningStyle'] = options.method
+
+project.save
+
+puts "Code signing was set to '#{options.method}'."
+
+plist = Xcodeproj::Plist.read_from_path('platforms/ios/barcode-test/barcode-test-Info.plist')
+plist['ITSAppUsesNonExemptEncryption'] = false unless plist['ITSAppUsesNonExemptEncryption']
+Xcodeproj::Plist.write_to_path(plist, 'platforms/ios/barcode-test/barcode-test-Info.plist')
