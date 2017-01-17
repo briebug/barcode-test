74f16d6ed740735692b7d174fa7c82534861e70d
James Stoup <james.stoup@yazdaapp.com>
2017-01-16 20:20:53 -0700

    updating to pass flag for plist location

--- fastlane/select_xcode_signing_method.rb
+++ fastlane/select_xcode_signing_method.rb
@@ -6,19 +6,20 @@ require 'xcodeproj'
 # ruby select_xcode_signing_method.rb -p <path/to/xcode_project> -t <Target> -m ['Automatic' | 'Manual']
 
 class Options
-  attr_accessor :path, :target, :method, :build_number
+  attr_accessor :path, :target, :method, :build_number, :plist_location
 
   def initialize(args)
-    @path = args[:path] || Dir.glob("*.xcodeproj").first
-    @target = args[:target]
-    @method = args[:method]
-    @build_number = args[:build_number]
+    @path           = args[:path] || Dir.glob("*.xcodeproj").first
+    @target         = args[:target]
+    @method         = args[:method]
+    @build_number   = args[:build_number]
+    @plist_location = args[:plist_location]
   end
 end
 
 def parse_args
   options_hash = {}
-  args = ARGV
+  args         = ARGV
   args.each_with_index do |arg, index|
     case arg
     when '--project_path', '-p'
@@ -31,6 +32,8 @@ def parse_args
       options_hash[:build_number] = args[index + 1]
     when '--target', '-t'
       options_hash[:target] = args[index + 1]
+    when '--plist-location', '-l',
+      options_hash[:plist_location] = args[index + 1]
     when '--signing_method', '-m'
       method = args[index + 1]
       unless ['Automatic', 'Manual'].include?(method)
@@ -47,17 +50,17 @@ options = Options.new parse_args
 
 project = Xcodeproj::Project.open(options.path)
 
-target_id = project.targets.select {|target| target.name == options.target }.first.uuid
-project.root_object.attributes['TargetAttributes'] = { target_id => { "ProvisioningStyle" => "Automatic" }} unless project.root_object.attributes.nil?
-attributes = project.root_object.attributes['TargetAttributes']
-target_attributes = attributes[target_id]
-target_attributes['ProvisioningStyle'] = options.method
+target_id                                          = project.targets.select { |target| target.name == options.target }.first.uuid
+project.root_object.attributes['TargetAttributes'] = { target_id => { "ProvisioningStyle" => "Automatic" } } unless project.root_object.attributes.nil?
+attributes                                         = project.root_object.attributes['TargetAttributes']
+target_attributes                                  = attributes[target_id]
+target_attributes['ProvisioningStyle']             = options.method
 
 project.save
 
 puts "Code signing was set to '#{options.method}'."
 
-plist = Xcodeproj::Plist.read_from_path('platforms/ios/barcode-test/barcode-test-Info.plist')
-plist['CFBundleVersion'] = options.build_number
+plist                                  = Xcodeproj::Plist.read_from_path(options.plist_location)
+plist['CFBundleVersion']               = options.build_number
 plist['ITSAppUsesNonExemptEncryption'] = false unless plist['ITSAppUsesNonExemptEncryption']
-Xcodeproj::Plist.write_to_path(plist, 'platforms/ios/barcode-test/barcode-test-Info.plist')
+Xcodeproj::Plist.write_to_path(plist, options.plist_location)
